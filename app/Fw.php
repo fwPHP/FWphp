6cb72935751a11573f170d3dfc7276e204312f8a
fwPHP <43908919+fwPHP@users.noreply.github.com>
2018-10-07 00:40:22 +0300

    Changed app/Fw.php

--- app/Fw.php
+++ app/Fw.php
@@ -7,8 +7,18 @@ class Fw {
     use core\Singleton;
 
     private function __construct() {
+        $this->_loading();
+    }
+
+    private function _loading() {
+        core\error\Handler::run();
         core\Route::run();
-        var_dump(RQ, RE);
+        define('APP', require 'tech' . D . 'app.php');
+        $this->_tech();
+    }
+
+    private function _tech() {
+        require 'tech' . D . 'error.php';
     }
 
 }
