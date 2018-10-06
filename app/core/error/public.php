157507cfe0257939b811fbfc24c9236a5f96df0d
fwPHP <43908919+fwPHP@users.noreply.github.com>
2018-10-07 00:34:50 +0300

    Add app/core/error/public.php

---
+++ app/core/error/public.php
@@ -0,0 +1,31 @@
+<?php
+
+echo '<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="google" content="notranslate">
+        <title>500 server error</title>
+        <link rel="icon" href="data:,">
+        <style>
+        html, body{
+            background: rgb(255, 255, 255);
+            font-family: \'Open Sans\', sans-serif;
+            line-height: 1.5;
+            color: rgb(55, 55, 55);
+        }
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        h1{
+            padding: 10px;
+            font-size: 16px;
+        }
+        </style>
+    </head>
+    <body>
+        <h1>500 server error</h1>
+    </body>
+</html>';
